1c0f904060a43ad3cc41e66a093174b41afd47bd
Rekuiem84 <morinijoshua@gmail.com>
2024-04-21 01:26:06 +0200

    chagement path

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -2,11 +2,7 @@
 function Card({ index = -1, nom = "nom du projet", href = "#", path = "/" }) {
 	return (
 		<a className="project-card" href={href}>
-			<img
-				className="projet__img"
-				src={`public/images/${path}.png`}
-				alt={nom}
-			/>
+			<img className="projet__img" src={`images/${path}.png`} alt={nom} />
 			<p className="projet__title">{index + 1 + " - " + nom}</p>
 		</a>
 	);
